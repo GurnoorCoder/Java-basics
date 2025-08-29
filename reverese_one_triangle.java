edb0cd34ec3a0fd6f33e36d1761363f098a86b38
GurnoorCoder <gurnoor.singh.cs28@iilm.edu>
2025-08-29 12:23:18 +0530

    Create reverese_one_triangle.java

---
+++ reverese_one_triangle.java
@@ -0,0 +1,15 @@
+public class NumberPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(1);
+            }
+            System.out.println();
+        }
+    }
+}
