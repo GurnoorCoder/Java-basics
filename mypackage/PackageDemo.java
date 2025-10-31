f54005ed9e371acd95ce4947f2d6507a4f76082d
GurnoorCoder <gurnoor.singh.cs28@iilm.edu>
2025-10-31 12:10:01 +0530

    Create PackageDemo.java

---
+++ mypackage/PackageDemo.java
@@ -0,0 +1,8 @@
+package mypackage;
+import mypackage.MyClass;
+public class PackageDemo {
+    public static void main(String[] args) {
+        MyClass obj=new MyClass();
+        System.out.println("Value from package: "+obj.x);
+    }
+}
